530e59c738f3e487edfb8ba818c36fd975721ae5
Bob Hunt <bob.hunt.email@gmail.com>
2018-07-30 11:38:12 -0400

    disabled vs code extension. rebooting vs code.

---
+++ 03_insert_user.sql
@@ -0,0 +1,3 @@
+insert user with:
+a username of 'admin'
+a bio of 'I am just a lonely administrator.'
---
+++ queries/02_create_posts_table.sql
@@ -0,0 +1,9 @@
+create a table named 'posts' with:
+an auto-incrementing id (serial primary key)
+a title no longer than 50 characters (varchar)
+a body no longer than 500 characters (varchar)
+a user_id that references a user by id (integer)
+
+CREATE TABLE IF NOT EXISTS posts (
+    id  
+)
